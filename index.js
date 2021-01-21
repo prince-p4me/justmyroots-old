cff99beeb213dadc7a6f53cb0b4e28f125f94828
prince <prince.p4me@gmail.com>
2021-01-22 00:56:54 +0530

    fixed now

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 import { AppRegistry } from "react-native";
 import App from "./src/app";
 import { name as appName } from "./app.json";
+import "babel-polyfill"
 
 AppRegistry.registerComponent(appName, () => App);
