bc0350f7cf303bfdfefbbe45751df49bbd0e035c
prince <prince.p4me@gmail.com>
2021-02-19 15:16:50 +0530

    app restart

--- src/View/Launch/Launch.container.js
+++ src/View/Launch/Launch.container.js
@@ -4,6 +4,9 @@ import shippingLocationActions from "../../Store/Redux/shippingLocation";
 import cartActions from "../../Store/Redux/cart";
 import LocationChangePage from "./LocationChange.page";
 import { Toast } from "native-base";
+import RNRestart from 'react-native-restart';
+import { Alert } from "react-native";
+
 class LocationChange extends Component {
   constructor(props) {
     super(props);
@@ -19,12 +22,45 @@ class LocationChange extends Component {
     this.props.shippingLocationsRequest();
   }
 
+  showALert = (location) => {
+    Alert.alert("Are you sure?",
+      "This will refresh the cart and restart the App",
+      [
+        {
+          text: "Cancel", onPress: () => {
+            // this.props.emptyCart();
+            // RNRestart.Restart();
+            console.log("cancel pressed");
+            this.props.navigation.navigate("TabNavigator");
+          }
+        },
+        {
+          text: "Okay", onPress: () => {
+            this.props.setShippingLocationRequest(location);
+            this.props.emptyCart();
+            Toast.show({ text: "Your application will restart in a while . . .", duration: 3000, buttonText: "Okay" });
+            setTimeout(() => {
+              RNRestart.Restart();
+            }, 4000)
+          }
+        }
+      ])
+  }
+
   shippingLocationSelected = location => {
-    Toast.show({ text: "Your cart has been refreshed", duration: 3000, buttonText: "Okay" });
-    this.props.setShippingLocationRequest(location);
-    this.props.emptyCart();
-    this.props.navigation.navigate("TabNavigator");
+    if (this.props.selectedShippingLocation.id) {
+      // Toast.show({ text: "Your application will restart . . .", duration: 3000, buttonText: "Okay" });
+      this.showALert(location);
+    } else if (this.props.sections && this.props.sections.length) {
+      Toast.show({ text: "Your cart has been refreshed . . .", duration: 3000, buttonText: "Okay" });
+      this.props.setShippingLocationRequest(location);
+      this.props.emptyCart();
+    } else {
+      this.props.setShippingLocationRequest(location);
+      this.props.navigation.navigate("TabNavigator");
+    }
   };
+
   closeLocationSelector = () => {
     this.props.navigation.navigate("TabNavigator");
   };
@@ -53,9 +89,10 @@ class LocationChange extends Component {
   }
 }
 
-const mapStateToProps = ({ shippingLocation }) => ({
+const mapStateToProps = ({ shippingLocation, cart }) => ({
   shippingLocations: shippingLocation.shippingLocations,
-  selectedShippingLocation: shippingLocation.selectedShippingLocation
+  selectedShippingLocation: shippingLocation.selectedShippingLocation,
+  sections: cart.sections
 });
 
 const mapDispatchToProps = { ...shippingLocationActions, ...cartActions };
